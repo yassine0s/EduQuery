cb3fbf6434a976097f46177a1b1bc1fd2612ed0b
Yassine <yassintoos@gmail.com>
2022-09-25 21:24:33 +0200

    body parser

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const mysql = require('mysql2')
 const dbconfig = require('./configs/database-config')
+const   bodyParser    = require('body-parser');
+const   cookieParser  = require('cookie-parser');
+
 pool = mysql.createPool(dbconfig.connection)
 const app = express();
 const adminRoute  = require('./routes/admin');
 const PORT = 5000; // Port of the backend server.
 
 
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 
 // setting up header content type
-app.use(function (req, res, next) {
-    req.headers['content-type'] = 'application/json';
-    next();
-  });
+// app.use(function (req, res, next) {
+//     req.headers['content-type'] = 'application/json';
+//     next();
+//   });
 
 
 // handling requests via router
