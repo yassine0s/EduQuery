90ebf1ff8e40b73746f2974b69af25e925c7800c
Yassine <yassintoos@gmail.com>
2022-09-29 20:11:41 +0200

    database modification + dummy data

--- Local deployment/config/db.sql
+++ Local deployment/config/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql:3306
--- Generation Time: Sep 25, 2022 at 08:18 PM
+-- Generation Time: Sep 29, 2022 at 06:10 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.0.19
 
@@ -44,6 +44,16 @@ CREATE TABLE `departments` (
   `name` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `name`) VALUES
+(1, 'Department of Automation and Applied Informatics'),
+(2, 'Department of Electronics Technology'),
+(3, 'Department of Networked Systems and Services'),
+(4, 'Department of Electron Devices');
+
 -- --------------------------------------------------------
 
 --
@@ -53,9 +63,19 @@ CREATE TABLE `departments` (
 CREATE TABLE `questions` (
   `id` int NOT NULL,
   `title` text NOT NULL,
-  `question` json NOT NULL,
-  `userid` int NOT NULL
+  `question` text NOT NULL,
+  `userid` int NOT NULL,
+  `departmentid` int NOT NULL,
+  `subjectid` int NOT NULL,
+  `category` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id`, `title`, `question`, `userid`, `departmentid`, `subjectid`, `category`) VALUES
+(1, 'question1', 'I have a quick question', 1, 1, 1, 'educational');
 
 -- --------------------------------------------------------
 
@@ -68,6 +88,13 @@ CREATE TABLE `subjects` (
   `name` text NOT NULL,
   `departmentid` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `name`, `departmentid`) VALUES
+(1, 'Automation and Applied Computer Science', 1);
 
 -- --------------------------------------------------------
 
@@ -99,7 +126,8 @@ INSERT INTO `users` (`id`, `username`, `firstname`, `lastname`, `email`, `type`)
 -- Indexes for table `answers`
 --
 ALTER TABLE `answers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `questionid` (`questionid`);
 
 --
 -- Indexes for table `departments`
@@ -111,7 +139,17 @@ ALTER TABLE `departments`
 -- Indexes for table `questions`
 --
 ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subjectid` (`subjectid`),
+  ADD KEY `departmentid` (`departmentid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `departmentid` (`departmentid`);
 
 --
 -- Indexes for table `users`
@@ -124,10 +162,52 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `answers`
+--
+ALTER TABLE `answers`
+  ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`questionid`) REFERENCES `questions` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`subjectid`) REFERENCES `subjects` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `questions_ibfk_2` FOREIGN KEY (`departmentid`) REFERENCES `departments` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `questions_ibfk_3` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD CONSTRAINT `subjects_ibfk_1` FOREIGN KEY (`departmentid`) REFERENCES `departments` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
